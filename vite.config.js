78728ed812b26b27944658f2313c179f86c4e780
Hidenori <hfu@users.noreply.github.com>
2025-05-10 23:37:35 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ export default defineConfig({
     host: true, // Allow access from other hosts
     proxy: {
       '/v4': {
-        target: 'http://localhost:3000',
+        target: 'http://127.0.0.1:3000', // localhost を IP アドレスに変更
         changeOrigin: true,
+        headers: {
+          'x-rewrite-url': '/v4',
+        },
         rewrite: (path) => path.replace(/^\/v4/, '/v4'),
       },
     },
